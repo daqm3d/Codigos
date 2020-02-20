1244e076f9fee42e8db452c2b96fe632557805af
deivy <deivy.u@hotmail.com>
2020-02-20 15:49:01 -0400

    incorporacion de comando para mysql

--- respaldo de BD/backup.bash
+++ respaldo de BD/backup.bash
@@ -1,26 +1,37 @@
 #!/bin/bash
-export FECHA=($(date +"%d-%m-%Y_%H:%M:%S") )# fecha de elaboración
-export NAME=RespaldoBDSigefirrhh_${FECHA}.backup # nombre del respaldo
-export DIR=/home/sigefirrhh/backup/ #direccion donde se guarda los documentos
-USER_DB=admin #usuario de la BD
-NAME_DB=nombre # nombre de la BD
-USER_GMAIL=@gmail.com #direccion de correo a enviar 
-MESSAGE_FILE=backup.mail.message # colocar este archivo en DIR.
-
-cd $DIR > ${NAME} #se habre direccion donde se guardara el backup
-export PGPASSWORD=xxxx #clave de la BD
-chmod 777 ${NAME} #se le da permisos al archivo
-echo "procesando la copia de la base de datos" #mensaje a ver 
-
-pg_dump -i -h (ip del servidor) -p (puerto) -U $USER_DB -F c -b -v -f ${NAME}  $NAME_DB # comando para crear el backup
-echo "backup terminado" >> $DIR/bitacora.log; #mensaje a ver + guardar log
-# usamos bzip2 para comprimir el sql
- bzip2 ${NAME}
- 
- # Enviar correo 
+export FECHA=($(date +"%d-%m-%Y_%H:%M:%S") )#FECHA DE ELABORACION 
+export NAME=RespaldoBDSigefirrhh_${FECHA}.backup #NOMBRE DEL RESPALDO
+export DIR=/home/sigefirrhh/backup/  #DIRECCION DONDE SE GUARDA LOS RESPALDOS
+export DIR2=/home/backup/ #DIRECCION GUARDARA LA BITACORA LOG
+USER_DB=admin #USUARIO DE LA BD
+NAME_DB=nombre #NOMBRE DE LA BD
+USER_GMAIL=@gmail.com #DIREECCION DE CORREO PARA NOTIFICAR  
+MESSAGE_FILE=backup.mail.message #MENSAJE A MOSTRAR EN EL CORREO
+#
+###     RESPALDO DE LA PRIMERA BD     ###
+cd $DIR > ${NAME} #SE HABRE DIRRECCION DONDE SE GUARDARA EL RESPALDO
+export PGPASSWORD=xxxx #CLAVE DE LA BD
+chmod 777 ${NAME} #SE LE DA PERMISOS AL ARCHIVO
+echo "procesando la copia de la base de datos" #MENSAJE A VER EN PROCESO
+pg_dump -i -h (ip del servidor) -p (puerto) -U $USER_DB -F c -b -v -f ${NAME}  $NAME_DB #COMANDO PARA CREAR EL BACKUP EN POSTGRES
+mysqldump  -u $USER_DB -p${PGPASSWORD}  $NAME_DB > ${NAME} #COMANDO PARA CREAR EL BACKUP EN MYSQL
+echo "backup terminado" >> $DIR/bitacora.log; #MENSAJE A VER CUANDO SE TERMINA 
+bzip2 ${NAME} #SE COMPRIME EL RESPALDO
+#
+###     ENVIO DE LOS RESPALDOS MEDIANTE SSH     ###
+scp -r $DIR root@IP_REMOTA:/backups/RESPALDO_BD #SE TIENE QUE CREAR UNA LLAVE PUBLICA DE SSH PARA QUE NO PIDA CONTRASEÑA
+#
+###     ENVIAR CORREO     ### 
 until
 mutt -s "Copia de seguridad BD ${NAME_DB}: $(date +"%d-%m-%Y")" ${USER_GMAIL} -a ${NAME}.bz2 < ${DIR}${MESSAGE_FILE} 
 do echo "   NO SE ENVIO CORREO REINTENTANDO ... " :; done
-echo "SE ENVIO CORREO"
-#Elimina archivos mayor a 30 dias
+echo "SE ENVIO CORREO" >> $DIR/bitacora.log; #MENSAJE A VER CUANDO SE ENVIA LOS RESPALDOS 
+#
+###     ELIMINAR ARCHIVOS CON MAS DE 30 DIAS     ###
 find ${DIR}*.bz2 -mtime +30 -exec rm {} \;
+
+### CREA UN CRONTAB -E CON: 
+# 30 23 * * * /home/backup/backup.bash 1> /dev/null 2> /home/backup/CRONTAB_BD.log
+# PARA AUTOMATIZAR EL RESPALDO
+## SE NESESITA INSTALAR: BZIP2 Y MUTT PARA CONPRIMIR Y ENVIAR EL CORREO 
+
